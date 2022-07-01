87611818e80d9173da2b3b1f6f5617902d362b3c
dev-hydrogen <96733109+dev-hydrogen@users.noreply.github.com>
2022-07-01 23:24:15 +0300

    change commands into hashmap

--- src/main/java/com/github/klainstom/microstom/commands/Commands.java
+++ src/main/java/com/github/klainstom/microstom/commands/Commands.java
@@ -2,7 +2,13 @@ package com.github.klainstom.microstom.commands;
 
 import net.minestom.server.command.builder.Command;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Commands {
-    public static final Command SHUTDOWN = new ShutdownCommand();
-    public static final Command RESTART = new RestartCommand();
+    public static final Map<String,Command> commands = new LinkedHashMap<>();
+    static {
+        commands.put("SHUTDOWN",new ShutdownCommand());
+        commands.put("RESTART",new RestartCommand());
+    }
 }
